3d59c1ca8a77a4fc38626da52d361c1b08818798
wdpmlgratis <wdpmlgratis@gmail.com>
2026-07-17 11:46:04 +0700

    update

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -4,7 +4,7 @@ export default defineAstroPaperConfig({
   site: {
     url: "https://astro-paper-wdp.pages.dev/",
     title: "WDP ML GRATIS | Ikuti Giveaway WDP ML gratis disini!",
-    description: "WDP ML GRATIS | Ikuti Giveaway WDP ML gratis disini!",
+    description: "WDPMLGRATIS | Ikuti Giveaway WDP ML gratis disini!",
     author: "WDP ML GRATIS",
     profile: "https://github.com/wdpmlgratis",
     ogImage: "default-og.jpg",
